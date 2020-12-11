adce96c2d6efac6f3a79ed327fc032bd13b2f3ff
kiku99 <elsd0326@gmail.com>
2020-12-11 17:28:49 +0900

    객체지향

--- src/hellowworld.java
+++ src/hellowworld.java
@@ -1,7 +1,32 @@
-public class hellowworld {
-    public  static  void main(String[] args){
-        String[] abc = {"1", "2", "3"};
 
-        System.out.println(abc[1]);
+class Calculator{
+    int left, right;
+
+    public void setOprands(int left, int right){
+        this.left = left;
+        this.right = right;
+    }
+
+    public void sum(){
+        System.out.println(this.left+this.right);
+    }
+
+    public void avg(){
+        System.out.println((this.left+this.right)/2);
+    }
+}
+
+public class hellowworld {
+    public static void main(String[] args) {
+
+        Calculator c1 = new Calculator();
+        c1.setOprands(10, 20);
+        c1.sum();
+        c1.avg();
+
+        Calculator c2 = new Calculator();
+        c2.setOprands(20, 40);
+        c2.sum();
+        c2.avg();
     }
 }
