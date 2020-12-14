c196662ab6b9e2a4bcd4a0acb20ad3091379ac92
kiku99 <elsd0326@gmail.com>
2020-12-14 21:52:03 +0900

    인터페이스와 다형성

--- src/hellowworld.java
+++ src/hellowworld.java
@@ -1,44 +1,25 @@
-abstract class Calculator{
-    int left, right;
-    public void setOprands(int left, int right){
-        this.left = left;
-        this.right = right;
-    }
-    int _sum() {
-        return this.left + this.right;
-    }
-    public abstract void sum();
-    public abstract void avg();
-    public void run(){
-        sum();
-        avg();
+interface father{}
+interface mother{}
+interface programmer{
+    public void coding();
+}
+interface believer{}
+class Steve implements father, programmer, believer{
+    public void coding(){
+        System.out.println("fast");
     }
 }
-class CalculatorDecoPlus extends Calculator {
-    public void sum(){
-        System.out.println("+ sum :"+_sum());
-    }
-    public void avg(){
-        System.out.println("+ avg :"+(this.left+this.right)/2);
+class Rachel implements mother, programmer{
+    public void coding(){
+        System.out.println("elegance");
     }
 }
-class CalculatorDecoMinus extends Calculator {
-    public void sum(){
-        System.out.println("- sum :"+_sum());
-    }
-    public void avg(){
-        System.out.println("- avg :"+(this.left+this.right)/2);
-    }
-}
-public class hellowworld {
-    public static void main(String[] args) {
-        Calculator c1 = new CalculatorDecoPlus();
-        c1.setOprands(10, 20);
-        c1.run();
+public class Workspace{
+    public static void main(String[] args){
+        programmer employee1 = new Steve();
+        programmer employee2 = new Rachel();
 
-        Calculator c2 = new CalculatorDecoMinus();
-        c2.setOprands(10, 20);
-        c2.run();
+        employee1.coding();
+        employee2.coding();
     }
-
 }
