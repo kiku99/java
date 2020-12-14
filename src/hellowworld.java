2c29319867fa476922ed23addc6b8125cebcfde1
kiku99 <elsd0326@gmail.com>
2020-12-14 21:21:21 +0900

    다형성

--- src/hellowworld.java
+++ src/hellowworld.java
@@ -1,15 +1,44 @@
-
-abstract class A{
-    public abstract int b();
-    //본체가 있는 메소드는 abstract 키워드를 가질 수 없다.
-    //public abstract int c(){System.out.println("Hello")}
-    //추상 클래스 내에는 추상 메소드가 아닌 메소드가 존재 할 수 있다.
-    public void d(){
-        System.out.println("world");
+abstract class Calculator{
+    int left, right;
+    public void setOprands(int left, int right){
+        this.left = left;
+        this.right = right;
+    }
+    int _sum() {
+        return this.left + this.right;
+    }
+    public abstract void sum();
+    public abstract void avg();
+    public void run(){
+        sum();
+        avg();
+    }
+}
+class CalculatorDecoPlus extends Calculator {
+    public void sum(){
+        System.out.println("+ sum :"+_sum());
+    }
+    public void avg(){
+        System.out.println("+ avg :"+(this.left+this.right)/2);
+    }
+}
+class CalculatorDecoMinus extends Calculator {
+    public void sum(){
+        System.out.println("- sum :"+_sum());
+    }
+    public void avg(){
+        System.out.println("- avg :"+(this.left+this.right)/2);
     }
 }
 public class hellowworld {
     public static void main(String[] args) {
-        A obj = new A();
+        Calculator c1 = new CalculatorDecoPlus();
+        c1.setOprands(10, 20);
+        c1.run();
+
+        Calculator c2 = new CalculatorDecoMinus();
+        c2.setOprands(10, 20);
+        c2.run();
     }
+
 }
